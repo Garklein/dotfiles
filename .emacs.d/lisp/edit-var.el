8959c98de5b69d9201234edc4e4d7266d1d3505a
Garklein <garklein97@gmail.com>
2024-09-22 15:53:51 -0400

    editvar kinda works, bar actually no cursor

--- .emacs.d/lisp/edit-var.el
+++ .emacs.d/lisp/edit-var.el
@@ -1,22 +1,26 @@
+;; todo: prompt with all variables (see how `describe-variable' does it)
+
+(defvar edit-var-buffers nil
+  "Associates buffers with a the symbol being edited.")
+(defun save-var (&rest _)
+  (when-let ((sym (alist-get (current-buffer) edit-var-buffers)))
+     (let ((new-val
+	    (->
+	     (buffer-substring-no-properties (point-min) (point-max))
+	     read-from-string car eval)))
+       (set sym new-val)
+       (setq edit-var-buffers (assoc-delete-all (current-buffer) edit-var-buffers))
+       (kill-buffer)
+       t)))
+(advice-add #'evil-write :before-until #'save-var)
+
 (defun edit-var (sym)
   (interactive "vVariable to edit: ")
   (let ((window (select-window (split-window-below)))
 	(buffer (switch-to-buffer (generate-new-buffer (concat "*`" (symbol-name sym) "' editing*")))))
-    (insert (format "%s" (symbol-value sym))) ;; todo fix
+    (insert (concat "`" (with-output-to-string (prin1 (symbol-value sym)))))
     (fit-window-to-buffer)
     (set-window-dedicated-p window t)
-
-    (defun save-var (_ &optional _)
-      (when (equal (current-buffer) buffer)
-	(let ((new-val
-	       (->
-		(buffer-substring-no-properties (point-min) (point-max))
-		read-from-string car eval)))
-	  (set sym new-val)
-	  (advice-remove #'evil-save #'save-var)
-	  (delete-window window)
-	  (kill-buffer)
-	  t)))
-    (advice-add #'evil-save :before-until #'save-var)))
+    (push (cons buffer sym) edit-var-buffers)))
 
 (provide 'edit-var)
