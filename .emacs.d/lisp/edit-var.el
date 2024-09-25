604cdd475b67f682524c19c9904188a7275a0204
Garklein <garklein97@gmail.com>
2024-09-25 09:06:06 -0400

    clean up edit-var

--- .emacs.d/lisp/edit-var.el
+++ .emacs.d/lisp/edit-var.el
@@ -1,26 +1,24 @@
-(require 'help-fns)
+(require 'thingatpt)
 
 (defvar edit-var-buffers nil
-  "Associates buffers with a the symbol being edited.")
+  "Associates variable editing buffers with the symbol being edited.")
 (defun save-var (&rest _)
   (when-let ((sym (alist-get (current-buffer) edit-var-buffers)))
-    (set sym (-> (buffer-string) read-from-string car eval))
+    (set sym (thread-first (buffer-string) read-from-string car eval))
     (kill-buffer)
     (setq edit-var-buffers (seq-filter #'buffer-live-p edit-var-buffers)) ; also remove any killed variable editing buffers
     t)) ; don't do the normal evil-write
 (advice-add #'evil-write :before-until #'save-var)
 
-;; stolen from `describe-variable'
+(defun variablep (symbol)
+  (and (symbolp symbol) (boundp symbol) (not (keywordp symbol))))
 (defun read-var-from-minibuffer ()
-  (let* ((v (variable-at-point))
-	 (inputted-var
-	  (completing-read
-	   (format-prompt "Variable to edit" (and (symbolp v) v))
-	   #'help--symbol-completion-table
-	   #'boundp
-	   t nil nil
-	   (if (symbolp v) (symbol-name v)))))
-    (intern inputted-var)))
+  (let* ((sym (symbol-at-point))
+	 (v (if (variablep sym) sym)))
+    (intern
+     (completing-read
+      (format-prompt "Variable to edit" v)
+      obarray #'variablep t nil nil v))))
 
 (defun edit-var (sym)
   (interactive (list (read-var-from-minibuffer)))
