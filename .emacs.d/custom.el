cff6d67ce405d8f767e2e2f8a778fd59f3f2186d
Garklein <garklein97@gmail.com>
2025-02-11 19:19:33 -0500

    java mode running

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -4,8 +4,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(safe-local-variable-values
-   '((eval setq indent-line-function
-	   (lambda nil (interactive) (insert "    "))))))
+   '((eval set-command
+	   (if (equal (buffer-name) "COMMIT_EDITMSG")
+	       (with-editor-finish 0)
+	     (compile "cd ~/garklein.github.io && ./build"))))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
