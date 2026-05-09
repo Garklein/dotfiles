701a2c7ad9a14e90cac73e769c3004d04294c8e1
Garklein <garklein97@gmail.com>
2026-05-09 14:54:01 -0400

    startx automatically

--- modules/home/programs/bash.nix
+++ modules/home/programs/bash.nix
@@ -4,6 +4,13 @@
     initExtra = ''
       PS1='[\t \W]$ '
     '';
+
+    # https://wiki.archlinux.org/title/Xinit#Autostart_X_at_login
+    profileExtra = ''
+      if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" -eq 1 ]; then
+         exec startx
+       fi
+     '';
   };
 
   home.sessionPath = [
