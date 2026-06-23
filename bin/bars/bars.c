4df3d82c031a9e65e2fc0c94ab7fa677e0924d9b
Garklein <garklein97@gmail.com>
2026-06-23 12:26:17 -0400

    .

--- bin/bars/bars.c
+++ bin/bars/bars.c
@@ -7,12 +7,10 @@
 #define MARG 12  /* margin */
 #define CORN 60  /* margin in corners */
 #define PERP 10  /* marker size */
-#define FORB B*b; for (int i=0; b=&bars[i],i<4; i++)
-
-#define SH(s,fmt,...) { FILE*fd=popen(s,"r"); fscanf(fd,fmt,__VA_ARGS__); pclose(fd); }
 
 enum { CLOCK, VOL, BATT, LIGHT };
-typedef struct bar { int x, y, w, h, l, id; xcb_drawable_t win; xcb_gcontext_t gc, cleargc; } B;
+typedef struct bar { int x,y,w,h,l,id; xcb_drawable_t win; xcb_gcontext_t gc,cleargc; } B;
+#define FORB B*b; for (int i=0; b=&bars[i],i<4; i++)
 B bars[4] =
 	{ (B){ .x=0,            .y=0,            .w=SCR_W,  .h=2*MARG, .l=SCR_W-2*CORN, .id=CLOCK }
 	, (B){ .x=SCR_W-2*MARG, .y=0,            .w=2*MARG, .h=SCR_H,  .l=SCR_H-2*CORN, .id=VOL }
@@ -20,25 +18,23 @@ B bars[4] =
 	, (B){ .x=0,            .y=0,            .w=2*MARG, .h=SCR_H,  .l=SCR_H-2*CORN, .id=LIGHT }
 	};
 
+#define SH(s,fmt,...) { FILE*fd=popen(s,"r"); fscanf(fd,fmt,__VA_ARGS__); pclose(fd); }
 xcb_connection_t *c;
-void line(B b,int x0, int y0, int dx, int dy) {
-	xcb_poly_line(c,XCB_COORD_MODE_PREVIOUS,b.win,b.gc,2,(const xcb_point_t []){{x0,y0},{dx,dy}}); }
-
+void line(B b,int x0,int y0,int dx,int dy) {
+	xcb_poly_line(c,XCB_COORD_MODE_PREVIOUS,b.win,b.gc,2,(xcb_point_t[]){{x0,y0},{dx,dy}}); }
+void mark(B b,int num,int denom,int size,int slant) { int pos=CORN+num*b.l/denom, offset=MARG-size/2;
+	b.w>b.h ? line(b,pos+slant,offset,-2*slant,size) : line(b,offset,pos+slant,size,-2*slant); }
 void draw() {
-	FORB { xcb_poly_fill_rectangle(c,b->win,b->cleargc,1,(const xcb_rectangle_t[]){0,0,b->w,b->h});
-		if (b->w>b->h) line(*b,CORN,MARG,b->l,0); else line(*b,MARG,CORN,0,b->l);
-		int muted, percent, status, hh, mm, ss, now, hour=60*60, halfDay=hour*12; switch (b->id) {
+	FORB { xcb_poly_fill_rectangle(c,b->win,b->cleargc,1,(xcb_rectangle_t[]){0,0,b->w,b->h});
+		b->w>b->h ? line(*b,CORN,MARG,b->l,0) : line(*b,MARG,CORN,0,b->l);
+		int muted,percent,status,hh,mm,ss,now, hour=60*60, halfDay=hour*12; switch (b->id) {
 		case CLOCK: SH("date +%H:%M:%S","%d:%d:%d",&hh,&mm,&ss); now=hh*hour+mm*60+ss;
-			line(*b,CORN+now%halfDay*b->l/halfDay,MARG-PERP/2,0,PERP);
-			line(*b,CORN+now%hour*b->l/hour,MARG-(PERP+4)/2,0,PERP+4); break;
-		case VOL: SH("sndioctl -n output.mute","%d",&muted);
-			if (!muted) { SH("sndioctl -n output.level","0.%d",&percent);
-				line(*b,MARG-PERP/2,CORN+b->l-percent*b->l/1000,PERP,0); } break;
-		case BATT: SH("apm -l","%d",&percent); SH("apm -b","%d",&status);
-			int offsetX=status==3?5:0; // 3 = charging
-			line(*b,CORN+(percent*b->l/100)+offsetX,MARG-PERP/2,-2*offsetX,PERP); break;
-		case LIGHT: SH("xbacklight -get","%d",&percent);
-			line(*b,MARG-PERP/2,CORN+b->l-b->l*percent/100,PERP,0);
+			mark(*b,now%halfDay,halfDay,PERP,0); mark(*b,now%hour,hour,PERP+4,0); break;
+		case VOL: SH("sndioctl -n output.level output.mute","0.%d\n%d",&percent,&muted);
+			if (!muted) mark(*b,1000-percent,1000,PERP,0); break;
+		case BATT: SH("apm -bl","%d\n%d",&status,&percent); // status 3 = charging
+			mark(*b,percent,100,PERP,(status==3)*4); break;
+		case LIGHT: SH("xbacklight -get","%d",&percent); mark(*b,100-percent,100,PERP,0);
 		} } xcb_flush(c); }
 
 int main() {
@@ -51,12 +47,12 @@ int main() {
 	FORB {
 		b->win=xcb_generate_id(c); xcb_create_window(c,32,b->win,scr->root,b->x,b->y,b->w,b->h,0,
 			XCB_WINDOW_CLASS_INPUT_OUTPUT,visual, XCB_CW_BACK_PIXEL|XCB_CW_BORDER_PIXEL|XCB_CW_COLORMAP,
-			(const uint32_t[]){0,0,colormap});
+			(int[]){0,0,colormap});
 		xcb_map_window(c, b->win); xcb_configure_window(c,b->win, // make cwm listen
-			XCB_CONFIG_WINDOW_X|XCB_CONFIG_WINDOW_Y,(const uint32_t[]){b->x,b->y});
-		xcb_change_property(c,XCB_PROP_MODE_REPLACE,b->win,XCB_ATOM_WM_NAME,XCB_ATOM_STRING,8,5,"rice");
+			XCB_CONFIG_WINDOW_X|XCB_CONFIG_WINDOW_Y,(int[]){b->x,b->y});
+		xcb_change_property(c,XCB_PROP_MODE_REPLACE,b->win,XCB_ATOM_WM_NAME,XCB_ATOM_STRING,8,4,"bars");
 		b->cleargc=xcb_generate_id(c); xcb_create_gc(c,b->cleargc,b->win,0,0); b->gc=xcb_generate_id(c);
 		xcb_create_gc(c,b->gc,b->win,XCB_GC_FOREGROUND|XCB_GC_LINE_WIDTH|XCB_GC_CAP_STYLE,
-			(const uint32_t[]){0xff504c45,5,XCB_CAP_STYLE_ROUND}); }
+			(int[]){0xff504c45,5,XCB_CAP_STYLE_ROUND}); }
 
 	for (struct timespec delay={.tv_nsec=1e9/4};;nanosleep(&delay,NULL)) draw(); }
